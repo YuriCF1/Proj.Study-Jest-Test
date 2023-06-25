ed8f55d4637465c058f3d2da1a1285889da5138b
Yuri Cruz <yuricruzf@gmail.com>
2023-06-25 19:54:28 -0300

    Fixing error, envolving form in RecoilRoot

--- sorteador-amigo-secreto/src/components/Formulario.test.tsx
+++ sorteador-amigo-secreto/src/components/Formulario.test.tsx
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
+import { RecoilRoot } from "recoil";
 
 //Testing-library = manipular a tela. Ex: const input
 //Quando se usa expect, é usado o JEST/JEST DOM
@@ -24,28 +25,31 @@ test("quando o input estiver vazio, novos participantes não devem ser adicionad
   expect(botao).toBeDisabled();
 });
 
-test("adicionar um participante, caso haja um nome preenchido",
-  () => {
-    render(<Formulario />);
-    const input = screen.getByPlaceholderText(
-      "Insira os nomes dos participantes"
-    );
-    const botao = screen.getByRole("button");
+test("adicionar um participante, caso haja um nome preenchido", () => {
+  render(
+    <RecoilRoot>
+      <Formulario />
+    </RecoilRoot>
+  );
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+  const botao = screen.getByRole("button");
 
-    //Inserir um valor no input
-    fireEvent.change(input, {
-      //Elemento que dispara o evento, e opções passadas
-      target: {
-        value: "Ana Catarina",
-      },
-    });
-
-    //CLicar no botão de submit
-    fireEvent.click(botao);
-
-    //Garantir que o input esteja com o foco ativo
-    expect(input).toHaveFocus();
-
-    //Garantir que o input não tenha um valor
-    expect(input).toHaveValue("");
+  //Inserir um valor no input
+  fireEvent.change(input, {
+    //Elemento que dispara o evento, e opções passadas
+    target: {
+      value: "Ana Catarina",
+    },
   });
+
+  //CLicar no botão de submit
+  fireEvent.click(botao);
+
+  //Garantir que o input esteja com o foco ativo
+  expect(input).toHaveFocus();
+
+  //Garantir que o input não tenha um valor
+  expect(input).toHaveValue("");
+});
