66d67090c168dcf58d024ba1b463106771ef28ca
Yuri Cruz <yuricruzf@gmail.com>
2023-06-26 23:59:17 -0300

    Verify if lista includes another name

--- sorteador-amigo-secreto/src/state/hooks/useAdicionarParticipante.ts
+++ sorteador-amigo-secreto/src/state/hooks/useAdicionarParticipante.ts
@@ -1,9 +1,15 @@
-import { useSetRecoilState } from "recoil"
-import { listaParticipanteState } from "../atom"
+import { useRecoilValue, useSetRecoilState } from "recoil"
+import { erroState, listaParticipanteState } from "../atom"
 
 export const useAdicionarParticipante = () => {
     const setLista = useSetRecoilState(listaParticipanteState)
+    const lista = useRecoilValue(listaParticipanteState)
+    const setError = useSetRecoilState(erroState)
     return (nomeDoParticipante: string) => {
+        if (lista.includes(nomeDoParticipante)) {
+            setError("Nomes duplicados não são permitidos")
+            return
+        }
         return setLista(listaAtual => [...listaAtual, nomeDoParticipante])
     }
 }
