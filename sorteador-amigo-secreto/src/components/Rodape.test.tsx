fc1aee52cb0fdede9784b11c4d00570fc2307ed4
Yuri Cruz <yuricruzf@gmail.com>
2023-07-05 21:56:29 -0300

    Creating navigate test

--- sorteador-amigo-secreto/src/components/Rodape.test.tsx
+++ sorteador-amigo-secreto/src/components/Rodape.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import Rodape from "./Rodape/Rodape";
@@ -7,6 +7,15 @@ import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes"
 jest.mock("../state/hooks/useListaDeParticipantes.ts", () => {
   return {
     useListaDeParticipantes: jest.fn(),
+  };
+});
+
+const mockNavegacao = jest.fn();
+
+jest.mock("react-router-dom", () => {
+  return {
+    //Para mockar qualquer outro hook que retorne uma função, tem que determinar dessa forma
+    useNavigate: () => mockNavegacao, //Chamando a função, pois o useNavigate está sendo invocado como função através de uma const
   };
 });
 
@@ -46,5 +55,20 @@ describe("Quando existir participantes o suficiente", () => {
     );
     const botao = screen.getByRole("button");
     expect(botao).not.toBeDisabled(); //Espera que o botão NÃO esteja habilitado
+  });
+
+  test("A brincadeira foi iniciada", () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>
+    );
+
+    const botao = screen.getByRole("button");
+    fireEvent.click(botao);
+
+    // expect(mockNavegacao).toHaveBeenCalled();
+    expect(mockNavegacao).toHaveBeenCalledTimes(1);
+    expect(mockNavegacao).toHaveBeenCalledWith("/sorteio");
   });
 });
