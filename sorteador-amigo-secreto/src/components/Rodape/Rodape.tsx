aa91762916075b88126878796f9fdf8ffd1f143e
Yuri Cruz <yuricruzf@gmail.com>
2023-07-05 21:56:13 -0300

    Using useNavigate

--- sorteador-amigo-secreto/src/components/Rodape/Rodape.tsx
+++ sorteador-amigo-secreto/src/components/Rodape/Rodape.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { useListaDeParticipantes } from '../../state/hooks/useListaDeParticipantes'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useListaDeParticipantes } from "../../state/hooks/useListaDeParticipantes";
+import { useNavigate } from "react-router-dom";
 
 const Rodape = () => {
-    const participantes = useListaDeParticipantes()
+  const participantes = useListaDeParticipantes();
 
-    // const navegarPara = useNavigate()
+  const navegarPara = useNavigate();
+
+  const iniciar = () => {
+    navegarPara("/sorteio");
+  };
 
   return (
     <footer>
-        <button disabled={participantes.length < 3}>Iniciar brincadeira</button>
+      <button disabled={participantes.length < 3} onClick={iniciar}>
+        Iniciar brincadeira
+      </button>
     </footer>
-  )
-}
+  );
+};
 
-export default Rodape
+export default Rodape;
