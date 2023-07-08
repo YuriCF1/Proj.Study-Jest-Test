81545bc23b1b7a0dd6f8379c4c4c6f692d4252ac
Yuri Cruz <yuricruzf@gmail.com>
2023-07-07 22:59:11 -0300

    Creting sortition and its test

---
+++ sorteador-amigo-secreto/src/Paginas/Sorteio.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { RecoilRoot } from "recoil";
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+import Sorteio from "./Sorteio";
+
+jest.mock("../state/hooks/useListaDeParticipantes", () => {
+  return {
+    useListaDeParticipantes: jest.fn(),
+  };
+});
+
+describe("Na pagin de sorteio", () => {
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+  });
+  const participantes = ["Ana", "Catarina", "Jorel"];
+
+  test("Todos os participntes podem exibir o seu amigo secreto", () => {
+    render(
+      <RecoilRoot>
+        <Sorteio />
+      </RecoilRoot>
+    );
+
+    const opcoes = screen.queryAllByRole("option");
+    expect(opcoes).toHaveLength(participantes.length);
+  });
+});
