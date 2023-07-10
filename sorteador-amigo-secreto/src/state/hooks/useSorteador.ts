192ccb0c2957457f9b7f520d5b03796bfa98b7f0
Yuri Cruz <yuricruzf@gmail.com>
2023-07-09 22:47:48 -0300

    Changing the logic to the hook

--- sorteador-amigo-secreto/src/state/hooks/useSorteador.ts
+++ sorteador-amigo-secreto/src/state/hooks/useSorteador.ts
@@ -1,7 +1,9 @@
-import shuffle from "just-shuffle"
+//OBS: TRANSFORMEI O HOOK EM UMA FUNÇÃO PURA(realizarSorteio), POIS AS FUNÇÕES PURAS PODEM SER TESTADAS. ENTÃO MOVI A REGRA DE NEGÓCIO PARA LÁ
+
 import { useListaDeParticipantes } from "./useListaDeParticipantes"
 import { useSetRecoilState } from "recoil"
 import { resultadoDoAmigoSecreto } from "../atom"
+import realizarSorteio from "../helpers/realizarSorteio"
 
 export const useSorteador = () => {
 
@@ -10,17 +12,22 @@ export const useSorteador = () => {
     const setResultado = useSetRecoilState(resultadoDoAmigoSecreto)
 
     return () => {
-        const totalDeParticipantes = participantes.length
-        const embaralhado = shuffle(participantes)
-
-        const resultado = new Map<string, string>()
-
-        for (let index = 0; index < totalDeParticipantes; index++) {
-            //index === 0, amigoSecreto 1, último será 0
-            const indiceDoAmigo = index === (totalDeParticipantes - 1) ? 0 : index + 1
-            resultado.set(embaralhado[index], embaralhado[indiceDoAmigo])
-        }
-
+        const resultado = realizarSorteio(participantes)
         setResultado(resultado)
     }
+
+    // return () => {
+    //     const totalDeParticipantes = participantes.length
+    //     const embaralhado = shuffle(participantes)
+
+    //     const resultado = new Map<string, string>()
+
+    //     for (let index = 0; index < totalDeParticipantes; index++) {
+    //         //index === 0, amigoSecreto 1, último será 0
+    //         const indiceDoAmigo = index === (totalDeParticipantes - 1) ? 0 : index + 1
+    //         resultado.set(embaralhado[index], embaralhado[indiceDoAmigo])
+    //     }
+
+    //     setResultado(resultado)
+    // }
 }
