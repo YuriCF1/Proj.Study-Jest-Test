ddfaa2424a488efb8c5327b1bd4f27f80854fa66
Yuri Cruz <yuricruzf@gmail.com>
2023-07-11 21:09:36 -0300

    Setting logic of shuffle, to apper on screen

--- sorteador-amigo-secreto/src/Paginas/Sorteio.tsx
+++ sorteador-amigo-secreto/src/Paginas/Sorteio.tsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+import { useResultadoDoSorteio } from "../state/hooks/useResultadoDoSorteio";
 
 const Sorteio = () => {
+  const [participanteDaVez, setParticipanteDaVez] = useState("");
+  const [amigoSecreto, setAmigoSecreto] = useState("");
+
   const participantes = useListaDeParticipantes();
+
+  const resultadoAmigoSecreto = useResultadoDoSorteio();
+
+  const sortear = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (resultadoAmigoSecreto.has(participanteDaVez)) {
+      setAmigoSecreto(resultadoAmigoSecreto.get(participanteDaVez)!); //Validando a existência evitando undefined
+    }
+  };
 
   return (
     <section>
-      <form action="">
-        <select name="participanteDaVez" id="participanteDaVez">
+      <form onSubmit={sortear}>
+        <select
+          required
+          name="participanteDaVez"
+          id="participanteDaVez"
+          placeholder="Selecione o seu nome"
+          value={participanteDaVez}
+          onChange={(e) => setParticipanteDaVez(e.target.value)}
+        >
           {participantes.map((participante) => (
             <option key={participante}>{participante}</option>
           ))}
         </select>
+        <button>Sorteador</button>
       </form>
+      {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
     </section>
   );
 };
