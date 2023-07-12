05c0544d57b56b6805a1111bddfb07b6de7c809f
Yuri Cruz <yuricruzf@gmail.com>
2023-07-11 21:10:37 -0300

    Testing if shuffle results is being shown on screen

--- sorteador-amigo-secreto/src/Paginas/Sorteio.test.tsx
+++ sorteador-amigo-secreto/src/Paginas/Sorteio.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
 import Sorteio from "./Sorteio";
+import { useResultadoDoSorteio } from "../state/hooks/useResultadoDoSorteio";
 
 jest.mock("../state/hooks/useListaDeParticipantes", () => {
   return {
@@ -10,11 +11,25 @@ jest.mock("../state/hooks/useListaDeParticipantes", () => {
   };
 });
 
+jest.mock("../state/hooks/useResultadoDoSorteio", () => {
+  return {
+    useResultadoDoSorteio: jest.fn(),
+  };
+});
+
 describe("Na pagin de sorteio", () => {
+  const participantes = ["Ana", "Catarina", "Jorel"];
+
+  const resultado = new Map([
+    ["Ana", "Jorel"],
+    ["Catarina", "Ana"],
+    ["Jorel", "Catarina"],
+  ]);
+
   beforeEach(() => {
     (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+    (useResultadoDoSorteio as jest.Mock).mockReturnValue(resultado);
   });
-  const participantes = ["Ana", "Catarina", "Jorel"];
 
   test("Todos os participntes podem exibir o seu amigo secreto", () => {
     render(
@@ -25,5 +40,29 @@ describe("Na pagin de sorteio", () => {
 
     const opcoes = screen.queryAllByRole("option");
     expect(opcoes).toHaveLength(participantes.length);
+  });
+
+  test("O amigo secreto é exibido quando solicitado", () => {
+    render(
+      <RecoilRoot>
+        <Sorteio />
+      </RecoilRoot>
+    );
+
+    const select = screen.getAllByPlaceholderText("Selecione o seu nome")[0];
+
+    fireEvent.change(select, {
+      target: {
+        value: participantes[0],
+      },
+    });
+
+    const botao = screen.getByRole("button");
+
+    fireEvent.click(botao);
+
+    const amigoSecreto = screen.getByRole("alert");
+
+    expect(amigoSecreto).toBeInTheDocument();
   });
 });
