41d3f1c8d6b2ef09ff13fe1567ec4ceb857dbfcc
Yuri Cruz <yuricruzf@gmail.com>
2023-07-14 22:33:31 -0300

    Adding shuffle result route

--- sorteador-amigo-secreto/src/App.tsx
+++ sorteador-amigo-secreto/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import Configuracao from "./Paginas/Configuracao";
+import Sorteio from "./Paginas/Sorteio";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <RecoilRoot>
           <Routes>
             <Route path="/" element={<Configuracao  />} />
+            <Route path="/sorteio" element={<Sorteio  />} />
           </Routes>
         </RecoilRoot>
       </BrowserRouter>
