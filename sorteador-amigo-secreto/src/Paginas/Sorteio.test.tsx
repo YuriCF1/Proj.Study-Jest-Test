53b9fd453e904c1c2accd704f9768d764cc66320
Yuri Cruz <yuricruzf@gmail.com>
2023-07-17 23:46:24 -0300

    Fixing test bc of the new option

--- sorteador-amigo-secreto/src/Paginas/Sorteio.test.tsx
+++ sorteador-amigo-secreto/src/Paginas/Sorteio.test.tsx
@@ -39,7 +39,7 @@ describe("Na pagin de sorteio", () => {
     );
 
     const opcoes = screen.queryAllByRole("option");
-    expect(opcoes).toHaveLength(participantes.length);
+    expect(opcoes).toHaveLength(participantes.length + 1); //Pois está vindo um option por padrão
   });
 
   test("O amigo secreto é exibido quando solicitado", () => {
