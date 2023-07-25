3465a86a87ba5f5f30ee4d92f5393fc5d89e841b
Yuri Cruz <yuricruzf@gmail.com>
2023-07-24 21:54:56 -0300

    Reorgazing

--- sorteador-amigo-secreto/src/components/Cabecalho/index.tsx
+++ sorteador-amigo-secreto/src/components/Cabecalho/index.tsx
@@ -1,13 +1,21 @@
 // src/componentes/Cabecalho/index.tsx
-import './estilos.css'
+import "./estilos.css";
 
 const Cabecalho = () => {
-    return (
-        <header className="cabecalho">
-            <div className="imagem-logo" role="img" aria-label='Logo do Sorteador'></div>
-            <img className='participante' src="./img/participante.png" alt="Participante com um presente na mão" />
-        </header>
-    )
-}
+  return (
+    <header className="cabecalho">
+      <div
+        className="imagem-logo"
+        role="img"
+        aria-label="Logo do Sorteador"
+      ></div>
+      <img
+        className="participante"
+        src="./img/participante.png"
+        alt="Participante com um presente na mão"
+      />
+    </header>
+  );
+};
 
-export default Cabecalho
+export default Cabecalho;
